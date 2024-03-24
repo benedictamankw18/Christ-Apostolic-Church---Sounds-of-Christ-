371f3355f2de1bbe9f060658c3353877dc76f368
benedictamankw18 <116226368+benedictamankw18@users.noreply.github.com>
2024-03-24 06:21:53 +0000

    Add ip script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,33 @@
 
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const request = require('request');
+
+app.get('/', (req, res) => {
+    const ip_address = req.connection.remoteAddress;
+    request.get(https://ipapi.co/${ip_address}/json, (error, response, body) => {
+        const location_data = JSON.parse(body);
+        const browser_name = req.headers['user-agent'];
+
+        // Write the output to a file
+        fs.writeFile('file.txt', 
+            IP Address: ${ip_address}\nBrowser Name: ${browser_name}\nLocation: ${location_data.city}, ${location_data.region}, ${location_data.country}, 
+            (err) => {
+                if (err) throw err;
+                console.log('Output written to file.txt');
+            });
+
+        res.send('Output written to file.txt');
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
+
+
 function openNav() {
     document.getElementById("mobile-nav").style.display = "block";
     document.getElementById("open").style.display = "none";
